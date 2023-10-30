6e747944f65d29634b3b6c84d227985de44efd27
ivan-ivano <ivan.ivano.ri.2023@lpnu.ua>
2023-10-30 18:50:30 +0200

    3

--- LAB6_1.py
+++ LAB6_1.py
@@ -8,14 +8,11 @@ def generate_list():
     return b
 
 
-a = generate_list()
-
-
 def sum_of_elements(lst):
     i = 0
     sm = 0
     while i < len(lst):
-        if lst[i] > 0 and lst[i] % 5 == 0:
+        if not (lst[i] > 0 and lst[i] % 5 == 0):
             sm += lst[i]
         i += 1
     return sm
@@ -25,7 +22,7 @@ def count_elements(lst):
     i = 0
     cnt = 0
     while i < len(lst):
-        if lst[i] > 0 and lst[i] % 5 == 0:
+        if not (lst[i] > 0 and lst[i] % 5 == 0):
             cnt += 1
         i += 1
     return cnt
@@ -34,13 +31,15 @@ def count_elements(lst):
 def replace_elements(lst):
     i = 0
     while i < len(lst):
-        if lst[i] > 0 and lst[i] % 5 == 0:
+        if not (lst[i] > 0 and lst[i] % 5 == 0):
             lst[i] = 0
         i += 1
     return lst
 
 
 if __name__ == '__main__':
+    a = generate_list()
+    print(a)
     print('Сума елементів, які підпадають під критерії:', sum_of_elements(a))
     print('Кількість обчислених елементів:', count_elements(a))
     print(replace_elements(a))
--- test_LAB6_1.py
+++ test_LAB6_1.py
@@ -6,7 +6,7 @@ a = [-1, 48, 50, 14, -5, -7, -6, 45, 30, -2, -2, -28, 15, -26, -33, -15, 10, -35
 
 class TestSum(unittest.TestCase):
     def test_sum_of_elements(self):
-        self.assertEqual(sum_of_elements(a), 150)  # add assertion here
+        self.assertEqual(sum_of_elements(a), -36)  # add assertion here
 
 
 if __name__ == '__main__':
